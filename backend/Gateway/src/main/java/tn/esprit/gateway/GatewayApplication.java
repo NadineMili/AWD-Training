d1eda4a61340a9aa2249cc956f29a2d7189a2f5d
NadineMili <mili.nadine07@gmail.com>
2026-04-22 16:10:38 +0100

    sc3

---
+++ backend/Gateway/src/main/java/tn/esprit/gateway/GatewayApplication.java
@@ -0,0 +1,29 @@
+package tn.esprit.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    //méthode nekhdem beha routing statiquement sans eureka
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+
+        return builder.routes().route("candidat", r -> r.path("/mic1/**").uri("lb://CANDIDAT"))
+               .route("job", r -> r.path("/mic2/**").uri("http://localhost:8082"))
+                .route("candidature", r -> r.path("/mic3/**").uri("http://localhost:8083"))
+                .route("meeting", r -> r.path("/mic5/**").uri("http://localhost:8085"))
+                .build();
+
+    }
+}
